5ab3140be4f839715532c3a6313db2c08b4fbe90
Manos Menioudakis <manos@Macmini.home>
2026-05-22 12:42:01 +0300

    Add match and rating history models

--- models.py
+++ models.py
@@ -17,3 +17,37 @@ class Player(Base):
     losses = Column(Integer, default=0)
 
     matches_played = Column(Integer, default=0)
+
+class Match(Base):
+    __tablename__ = "matches"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    player_a_id = Column(Integer)
+    player_b_id = Column(Integer)
+
+    score_a = Column(Integer)
+    score_b = Column(Integer)
+
+    race_to = Column(Integer)
+
+    game_type = Column(String)
+
+    winner_id = Column(Integer)
+
+
+class RatingHistory(Base):
+    __tablename__ = "rating_history"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    player_id = Column(Integer)
+    match_id = Column(Integer)
+
+    old_rating = Column(Integer)
+    new_rating = Column(Integer)
+
+    rating_change = Column(Integer)
+
+    category_before = Column(String)
+    category_after = Column(String)
