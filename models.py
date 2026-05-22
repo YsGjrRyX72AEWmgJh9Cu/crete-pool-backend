b207729a8360efa219b89d078afb51d7f95c67d2
Manos Menioudakis <manos@Macmini.home>
2026-05-22 11:30:23 +0300

    Add models

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Player(Base):
+    __tablename__ = "players"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+    current_rating = Column(Integer, default=1000)
+    matches_played = Column(Integer, default=0)
