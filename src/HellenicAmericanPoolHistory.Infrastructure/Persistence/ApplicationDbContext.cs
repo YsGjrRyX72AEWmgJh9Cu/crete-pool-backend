a82937064984a9de1d2940bcd28d7a257ed4d804
Manos Menioudakis <manos.menioudakis@gmail.com>
2026-07-17 10:48:00 +0300

    feat(infrastructure): add application db context

---
+++ src/HellenicAmericanPoolHistory.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+using HellenicAmericanPoolHistory.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HellenicAmericanPoolHistory.Infrastructure.Persistence;
+
+/// <summary>
+/// Represents the application's database context.
+/// </summary>
+public sealed class ApplicationDbContext : DbContext
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ApplicationDbContext"/> class.
+    /// </summary>
+    /// <param name="options">The database context options.</param>
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Player> Players => Set<Player>();
+
+    public DbSet<Tournament> Tournaments => Set<Tournament>();
+
+    public DbSet<Participation> Participations => Set<Participation>();
+
+    public DbSet<Match> Matches => Set<Match>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ArgumentNullException.ThrowIfNull(modelBuilder);
+
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
